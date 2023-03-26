a29a6c6fbdfffeea0cc4f6e91a68621d5e6d9b96
Eduardo Farinati <eduardo.farinati@ufrgs.br>
2023-03-25 22:25:45 -0300

    Add main helper scripts

---
+++ main.bat
@@ -0,0 +1,3 @@
+rem This script is a helper to easily call
+rem the main.py file with a python interpreter
+python src\main.py %*
---
+++ main.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# This script is a helper to easily call
+# the main.py file with a python interpreter
+/usr/bin/env python3 -B ./src/main.py "$@"
