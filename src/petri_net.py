EduardoFarinati/FlexFact-Tina

commit 01a5fea6c6339134ec0aeb807c64d3b3ed1bc307

Use a class for places in the Petri net

src/petri_net.py
@@ -1,13 +1,21 @@
 from dataclasses import dataclass, field
 from enum import Enum, auto
-from typing import Dict, List
+from typing import List, Union
+
+
+@dataclass
+class Place:
+    """Representation of a Petri net place, with its number of tokens."""
+
+    name: str
+    tokens: int
 
 
 @dataclass
 class Arc:
     """Representation of an arc."""
 
-    place: str
+    place: Place
     weight: int
 
 
@@ -30,12 +38,12 @@ class InputArc(Arc):
 
     type: InputArcTypes
 
-    def is_met(self, tokens: int) -> bool:
+    def is_met(self) -> bool:
         """Check if the arc preconditions are met."""
         if self.type == InputArcTypes.INHIBITOR:
-            return tokens < self.weight
+            return self.place.tokens < self.weight
 
-        return tokens >= self.weight
+        return self.place.tokens >= self.weight
 
 
 @dataclass
@@ -48,14 +56,73 @@ class Transition:
     input_arcs: List[InputArc] = field(default_factory=list)
     output_arcs: List[OutputArc] = field(default_factory=list)
 
-    def is_enabled(self, places: Dict[str, int]) -> bool:
+    def is_enabled(self) -> bool:
         """Check if the transition is enabled, by checking each arc
         preconditions are met (ie. if there are enough tokens
         present at the source place)."""
 
         for input_arc in self.input_arcs:
-            tokens = places[input_arc.place]
-            if not input_arc.is_met(tokens):
+            if not input_arc.is_met():
                 return False
 
         return True
+
+
+@dataclass
+class PetriNet:
+    places: List[Place]
+    transitions: List[Transition]
+
+    def __init__(
+        self,
+        places: Union[List[Place], None] = None,
+        transitions: Union[List[Transition], None] = None,
+    ):
+        if places is None:
+            places = []
+        if transitions is None:
+            transitions = []
+
+        self.places = places
+        self.transitions = transitions
+
+        # Set mapping from places and transitions
+        self._place_mapping = {place.name: i for i, place in enumerate(places)}
+        self._transition_mapping = {
+            transition.name: i for i, transition in enumerate(transitions)
+        }
+
+    def add_place(self, place: Place):
+        i = self._place_mapping.get(place.name)
+        if i:
+            # Overwrites place if it is
+            # already added in the network
+            self.places[i] = place
+        else:
+            self._place_mapping[place.name] = len(self.places)
+            self.places.append(place)
+
+    def add_transition(self, transition: Transition):
+        i = self._transition_mapping.get(transition.name)
+        if i:
+            # Overwrites transition if it is
+            # already added in the network
+            self.transitions[i] = transition
+        else:
+            self._transition_mapping[transition.name] = len(self.transitions)
+            self.transitions.append(transition)
+
+    def get_place(self, name: str) -> Union[Place, None]:
+        i = self._place_mapping.get(name)
+        if i:
+            return self.places[i]
+        return None
+
+    def get_transition(self, name: str) -> Union[Transition, None]:
+        i = self._transition_mapping.get(name)
+        if i:
+            return self.transitions[i]
+        return None
+
+    def get_marking(self) -> List[int]:
+        return [place.tokens for place in self.places]

src/controller.py
@@ -1,9 +1,10 @@
-from typing import Dict, List, Tuple
+from typing import Dict, Tuple
 from pymodbus.exceptions import ModbusException
 
-from parsers.network import InputArcTypes, Transition
+from parsers.network import InputArcTypes
 from parsers.device import InputEvent, OutputEvent
 from modbus_client import ModbusClient
+from petri_net import PetriNet
 from special_tokens import strip_name, COMMENT
 
 
@@ -20,14 +21,12 @@ class Controller:
     def __init__(
         self,
         address: Tuple[str, int],
-        transitions: List[Transition],
-        places: Dict[str, int],
+        petri_network: PetriNet,
         inputs: Dict[str, InputEvent],
         outputs: Dict[str, OutputEvent],
     ):
         self.client = ModbusClient(address)
-        self.transitions = transitions
-        self.places = places
+        self.petri_network = petri_network
         self.inputs = inputs
         self.outputs = outputs
 
@@ -98,7 +97,7 @@ class Controller:
         self.read_all()
 
         # Iterate through all the transitions in a network
-        for transition in self.transitions:
+        for transition in self.petri_network.transitions:
             # Check if the FlexFact requirements are met (ie. if a sensor has
             # just turn on or off). Adding ; to the start of a name means it
             # is ignored and the transition is allowed
@@ -124,7 +123,7 @@ class Controller:
                 did_transition = True
 
             # If all requirements are met
-            if transition.is_enabled(self.places) and did_transition:
+            if transition.is_enabled() and did_transition:
                 print(f"  t: {transition.name}")
 
                 # Move the tokens around
@@ -135,12 +134,13 @@ class Controller:
                     _type = input_arc.type
 
                     if _type == InputArcTypes.REGULAR:
-                        self.places[place] -= weight
+                        place.tokens -= weight
+
                 for output_arc in transition.output_arcs:
                     place = output_arc.place
                     weight = output_arc.weight
 
-                    self.places[place] += weight
+                    place.tokens += weight
 
                 # Issue the commands via Modbus. Note that ; is also used to
                 # separate between command in the name, so you can use less

src/main.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python3
 import time
-from typing import Dict, List, Tuple
+from typing import Tuple
 
 import cli
 from parsers import network, device
 from controller import Controller
+from petri_net import PetriNet
 
 
 RECONNECT_PERIOD_S = 1.5
 
 
-def header_message(
-    address: Tuple[str, int],
-    transitions: List[network.Transition],
-    places: Dict[str, int],
-):
+def header_message(address: Tuple[str, int], petri_network: PetriNet):
     print(f"Controlling FlexFact plant at {address}, with:")
-    print(f"  Transitions: {len(transitions)}")
-    print(f"  Places: {len(places)}")
+    print(f"  Transitions: {len(petri_network.transitions)}")
+    print(f"  Places: {len(petri_network.places)}")
     print("")
 
 
@@ -39,15 +36,15 @@ def main():
     device_path, network_path, sleep_period = cli.get_args()
 
     # Parse config
-    transitions, places = network.parse(network_path)
+    petri_network = network.parse(network_path)
     address, inputs, outputs = device.parse(device_path)
 
     # Start controller
     while True:
-        header_message(address, transitions, places)
+        header_message(address, petri_network)
         try:
             with Controller(
-                address, transitions, places, inputs, outputs
+                address, petri_network, inputs, outputs
             ) as controller:
                 print("Operation display:")
                 while True:

src/parsers/network.py
@@ -1,16 +1,19 @@
-from typing import Dict, List, Tuple, Union
 from pathlib import Path
 
-from petri_net import InputArc, InputArcTypes, OutputArc, Transition
+from petri_net import (
+    InputArc,
+    InputArcTypes,
+    OutputArc,
+    PetriNet,
+    Place,
+    Transition,
+)
 
 
-def parse(
-    filepath: Union[Path, str]
-) -> Tuple[List[Transition], Dict[str, int]]:
+def parse(filepath: Path) -> PetriNet:
     """Parse the text file generated by tina export."""
 
-    transitions = []
-    places = {}
+    network = PetriNet()
 
     with open(filepath, "r") as file:
         for line in file:
@@ -22,6 +25,7 @@ def parse(
                 .replace("}", "")
                 .split()
             )
+
             if len(elements) == 0:
                 continue
             elif elements[0] == "tr":
@@ -29,53 +33,83 @@ def parse(
                 arrow_index = elements.index("->")
                 for element in elements[3:arrow_index]:
                     if "?" in element:
-                        parts = element.split("?")
-                        if int(parts[1]) > 0:
+                        place_name, weight = element.split("?", 1)
+
+                        place = network.get_place(place_name)
+                        if not place:
+                            place = Place(place_name, 0)
+                            network.add_place(place)
+
+                        if int(weight) > 0:
                             transition.input_arcs.append(
                                 InputArc(
-                                    parts[0], int(parts[1]), InputArcTypes.READ
+                                    place,
+                                    int(weight),
+                                    InputArcTypes.READ,
                                 )
                             )
                         else:
                             transition.input_arcs.append(
                                 InputArc(
-                                    parts[0],
-                                    abs(int(parts[1])),
+                                    place,
+                                    abs(int(weight)),
                                     InputArcTypes.INHIBITOR,
                                 )
                             )
                     else:
                         parts = element.split("*")
+
+                        place_name = parts[0]
+                        place = network.get_place(place_name)
+                        if not place:
+                            place = Place(place_name, 0)
+                            network.add_place(place)
+
                         if len(parts) == 1:
                             transition.input_arcs.append(
-                                InputArc(parts[0], 1, InputArcTypes.REGULAR)
+                                InputArc(place, 1, InputArcTypes.REGULAR)
                             )
                         else:
+                            weight = parts[1]
                             transition.input_arcs.append(
                                 InputArc(
-                                    parts[0],
-                                    int(parts[1]),
+                                    place,
+                                    int(weight),
                                     InputArcTypes.REGULAR,
                                 )
                             )
+
                 if arrow_index + 1 != len(elements):
                     for element in elements[arrow_index + 1 :]:
                         parts = element.split("*")
-                        places[parts[0]] = 0
+
+                        place_name = parts[0]
+                        place = network.get_place(place_name)
+                        if not place:
+                            place = Place(place_name, 0)
+                            network.add_place(place)
+
                         if len(parts) == 1:
                             transition.output_arcs.append(
-                                OutputArc(parts[0], 1)
+                                OutputArc(place, 1)
                             )
                         else:
+                            weight = parts[1]
                             transition.output_arcs.append(
-                                OutputArc(parts[0], int(parts[1]))
+                                OutputArc(place, int(weight))
                             )
 
-                transitions.append(transition)
+                network.add_transition(transition)
 
             elif elements[0] == "pl":
-                places[elements[1]] = int(
-                    elements[2].replace("(", "").replace(")", "")
-                )
+                place_name = elements[1]
+                marking = int(elements[2].replace("(", "").replace(")", ""))
 
-    return transitions, places
+                place = network.get_place(place_name)
+                if not place:
+                    place = Place(place_name, marking)
+                    network.add_place(place)
+                else:
+                    place.tokens = marking
+
+    return network

commit 235c9a74d1344308409abd1d807ec245ac573204

Fix first place/transition regression in commit 01a5fea6c6339134ec0aeb807c64d3b3ed1bc307

src/petri_net.py
@@ -146,9 +146,8 @@ class PetriNet:
 
     def add_place(self, place: Place):
         i = self._place_mapping.get(place.name)
-        if i:
-            # Overwrites place if it is
-            # already added in the network
+        if i is not None:
+            # Overwrites place if it is already present in the network
             self.places[i] = place
         else:
             self._place_mapping[place.name] = len(self.places)
@@ -156,9 +155,8 @@ class PetriNet:
 
     def add_transition(self, transition: Transition):
         i = self._transition_mapping.get(transition.name)
-        if i:
-            # Overwrites transition if it is
-            # already added in the network
+        if i is not None:
+            # Overwrites transition if it is already present in the network
             self.transitions[i] = transition
         else:
             self._transition_mapping[transition.name] = len(self.transitions)
@@ -166,15 +164,17 @@ class PetriNet:
 
     def get_place(self, name: str) -> Union[Place, None]:
         i = self._place_mapping.get(name)
-        if i:
-            return self.places[i]
-        return None
+        if i is None:
+            return None
+
+        return self.places[i]
 
     def get_transition(self, name: str) -> Union[Transition, None]:
         i = self._transition_mapping.get(name)
-        if i:
-            return self.transitions[i]
-        return None
+        if i is None:
+            return None
+
+        return self.transitions[i]
 
     def get_marking(self) -> List[int]:
         return [place.tokens for place in self.places]